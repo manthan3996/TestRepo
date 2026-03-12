3c40a8cbdbed5985a92ee83e2a6c03f168887a85
Manthan <manthan3996@gmail.com>
2026-03-12 11:37:02 +0530

    First Commit

---
+++ playwright.config.js
@@ -0,0 +1,40 @@
+// @ts-chec
+import {chromium,defineConfig,devices} from '@playwright/test';
+import { config } from 'node:process';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout : 40000,
+  expect :{
+    timeout:10000
+  },
+reporter : 'html',
+  /* Configure projects for major browsers */
+screenshot : 'only-on-failure',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'],
+        headless:false
+       }
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
---
+++ test-results/ChildWindowsTest-Child-Windows-Test-chromium/error-context.md
@@ -0,0 +1,28 @@
+# Page snapshot
+
+```yaml
+- link "Free Access to InterviewQues/ResumeAssistance/Material":
+  - /url: https://rahulshettyacademy.com/documents-request
+- link "QA Meetup with Rahul Shetty @Pune - Limited Seats! Book Now!":
+  - /url: https://qasummit.org/
+- heading [level=3]:
+  - img
+- text: "Username:"
+- textbox "Username:"
+- text: "Password:"
+- textbox "Password:"
+- text: Admin
+- radio "Admin" [checked]
+- text: User
+- radio "User"
+- combobox:
+  - option "Student" [selected]
+  - option "Teacher"
+  - option "Consultant"
+- checkbox "I Agree to the terms and conditions"
+- text: I Agree to the
+- link "terms and conditions":
+  - /url: "#"
+- button "Sign In"
+- paragraph: (username is rahulshettyacademy and Password is learning)
+```
